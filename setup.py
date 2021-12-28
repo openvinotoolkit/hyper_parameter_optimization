90d1baa83b726f07e657baec609cf4327f37f04a
Eddy Kim <eddy.kim@intel.com>
2021-12-28 19:00:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hpopt',
-    version='0.0.1',
+    version='0.1.0',
     url='https://github.com/openvinotoolkit/hyper_parameter_optimization',
     packages=find_packages(),
     description='Hyper-parameters Optimization',
