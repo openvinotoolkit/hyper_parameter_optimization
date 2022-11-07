32c0a2e897f19e6b99109c36c6c8df656b6feef9
eunwoosh <eunwoo.shin@intel.com>
2022-11-07 09:54:14 +0900

    clear up unused file

--- hpopt/__init__.py
+++ hpopt/__init__.py
@@ -2,19 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from hpopt.hpo_ui import (
-    get_status_path,
-    get_trial_path,
-    load_json,
-)
-
 from hpopt.hpo_runner import run_hpo_loop
 from hpopt.hpo_base import TrialStatus
 
 __all__ = [
-    "get_status_path",
-    "get_trial_path",
-    "load_json",
     "run_hpo_loop",
     "TrialStatus",
 ]
